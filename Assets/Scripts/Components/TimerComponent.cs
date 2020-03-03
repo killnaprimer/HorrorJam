2b2b7b9f64fa97881e9595e01d22504a48e96b29
Killnaprimer <kill.naprimer@gmail.com>
2020-03-03 19:05:04 +0300

    Fixes

--- Assets/Scripts/Components/TimerComponent.cs
+++ Assets/Scripts/Components/TimerComponent.cs
@@ -3,32 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractiveComponent : MonoBehaviour
+public class TimerComponent : MonoBehaviour
 {
-    public AudioSource audio;
-
     public GameObject listener;
-    // Start is called before the first frame update
-
-    public void PlaySound()
-    {
-        if (audio != null)
-        {
-            audio.Play();
-        }
-    }
+    public float delay;
     
     public void Interact(GameObject instigator)
     {
-        GetComponent<Animator>().SetTrigger("onOpen");
-        if (listener == null) return;
+        StartCoroutine(InteractOnDelay());
+    }
+    
+    IEnumerator InteractOnDelay()
+    {
+        yield return new WaitForSeconds(delay);
         listener.SendMessage("Interact", this.gameObject, SendMessageOptions.DontRequireReceiver);
     }
-
+    
     private void OnDrawGizmosSelected()
     {
         if (listener == null) return;
         Gizmos.color = new Color(1f, 0.7f, 0f, 0.5f);
         Gizmos.DrawLine(transform.position, listener.transform.position);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(transform.position, "timer",true);
     }
 }
